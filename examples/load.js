9492adae6d50e2dfe1441abdc5281e50da6f7559
Matt Styles <matt@veryfizzyjelly.com>
2015-09-21 13:37:38 +0100

    #add use fetch to load image data

--- examples/load.js
+++ examples/load.js
@@ -6,15 +6,24 @@ import ImageLoader from '../lib'
 let preloader = new Preloader()
 preloader.register( new ImageLoader() )
 
-preloader.load( 'http://fillmurray.com/100/100?jpg' )
-preloader.load( 'http://fillmurray.com/200/200?jpg' )
-preloader.load( './awesomes.jpg' )
+// preloader.load( 'http://fillmurray.com/100/100?jpg' )
+// preloader.load( 'http://fillmurray.com/200/200?jpg' )
+preloader.load( './awesome404.jpg' )
+
+for ( let i = 0; i < 3; i++ ) {
+    preloader.load( './awesome.jpg?' + ( Math.random() * 10000 ) + '&jpg' )
+}
+let start = performance.now()
 
 preloader.on( EVENTS.LOAD, event => {
-    console.log( '-- load' )
+    console.log( '-- load', performance.now() )
+    console.log( event )
+})
+preloader.on( EVENTS.LOAD_ERROR, event => {
+    console.log( '** error', performance.now() )
     console.log( event )
 })
 preloader.on( EVENTS.COMPLETE, res => {
-    console.log( '-- COMPLETE' )
+    console.log( '-- COMPLETE', performance.now() - start + 'ms' )
     console.log( res )
 })
--- src/index.js
+++ src/index.js
@@ -2,32 +2,55 @@
 import 'whatwg-fetch'
 import { EVENTS } from 'preload.io'
 
+
+class IOError extends Error {
+    constructor( opts ) {
+        super( opts.message )
+
+        this.name = 'IOError'
+        this.stack = ( new Error() ).stack
+
+        Object.keys( opts ).forEach( key => {
+            this[ key ] = opts[ key ]
+        })
+    }
+}
+
+
 export default class ImageLoader {
     constructor() {
         this.name = 'imageLoader'
         this.match = /jpg$|jpeg$|png$/
     }
 
-    load( ctx, opts ) {
-        let image = new Image()
-        image.addEventListener( 'load', event => {
+    async load( ctx, opts ) {
+        // @TODO optionally use old school tag loading
+        try {
+            let res = await fetch( opts.url )
+                .then( response => {
+                    if ( response.status >=200 && response.status < 300 ) {
+                        return response
+                    }
+
+                    throw new IOError({
+                        message: response.statusText,
+                        status: response.status
+                    })
+                })
+
+            let blob = await res.blob()
+
             ctx.emit( EVENTS.LOAD, {
                 id: opts.id,
-                status: 200,
-                res: event
+                status: res.status,
+                res: blob
             })
-        })
-        image.addEventListener( 'error', event => {
-            ctx.emit( EVENTS.LOAD, {
-                id: opts.id,
-                status: 500,
-                error: event
-            })
+        } catch( err ) {
             ctx.emit( EVENTS.LOAD_ERROR, {
                 id: opts.id,
-                error: event
+                status: err.status,
+                res: err
             })
-        })
-        image.src = opts.url
+        }
     }
 }
